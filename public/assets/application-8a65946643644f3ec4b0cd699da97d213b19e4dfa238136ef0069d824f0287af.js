a67c6316ce3c040e7699f644c78a96c63bb1d0a8
mattgellert <mattgellert@gmail.com>
2017-12-14 22:21:00 -0500

    test4

--- public/assets/application-8a65946643644f3ec4b0cd699da97d213b19e4dfa238136ef0069d824f0287af.js
+++ public/assets/application-8a65946643644f3ec4b0cd699da97d213b19e4dfa238136ef0069d824f0287af.js
@@ -1366,25 +1366,25 @@ return t.dispatch("turbolinks:before-render",{data:{newBody:e}})},r.prototype.no
 
 
 }).call(this);
-function updateWidth() {
-  document.getElementById("window-width").innerHTML = document.documentElement.clientWidth;
-}
-window.addEventListener("resize", updateWidth);
-updateWidth();
-try {
-  var mp3 = "data:audio/mpeg;base64,/+MYxAAAAANIAUAAAASEEB/jwOFM/0MM/90b/+RhST//w4NFwOjf///PZu////9lns5GFDv//l9GlUIEEIAAAgIg8Ir/JGq3/+MYxDsLIj5QMYcoAP0dv9HIjUcH//yYSg+CIbkGP//8w0bLVjUP///3Z0x5QCAv/yLjwtGKTEFNRTMuOTeqqqqqqqqqqqqq/+MYxEkNmdJkUYc4AKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
-  var audio = new Audio();
-  audio.autoplay = true;
-  audio.volume = 0;
-  audio.addEventListener("play", function() {
-    document.querySelector("html").classList.add("autoplay");
-    document.querySelector("html").classList.remove("no-autoplay");
-  });
-  audio.src = mp3;
-} catch (e) {
-  document.getElementById("autoplay").innerHTML = e;
-}
-;
+// function updateWidth() {
+//   document.getElementById("window-width").innerHTML = document.documentElement.clientWidth;
+// }
+// window.addEventListener("resize", updateWidth);
+// updateWidth();
+// try {
+//   var mp3 = "data:audio/mpeg;base64,/+MYxAAAAANIAUAAAASEEB/jwOFM/0MM/90b/+RhST//w4NFwOjf///PZu////9lns5GFDv//l9GlUIEEIAAAgIg8Ir/JGq3/+MYxDsLIj5QMYcoAP0dv9HIjUcH//yYSg+CIbkGP//8w0bLVjUP///3Z0x5QCAv/yLjwtGKTEFNRTMuOTeqqqqqqqqqqqqq/+MYxEkNmdJkUYc4AKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
+//   var audio = new Audio();
+//   audio.autoplay = true;
+//   audio.volume = 0;
+//   audio.addEventListener("play", function() {
+//     document.querySelector("html").classList.add("autoplay");
+//     document.querySelector("html").classList.remove("no-autoplay");
+//   });
+//   audio.src = mp3;
+// } catch (e) {
+//   document.getElementById("autoplay").innerHTML = e;
+// }
+// ;
 /*!
  * jQuery JavaScript Library v1.12.4
  * http://jquery.com/
